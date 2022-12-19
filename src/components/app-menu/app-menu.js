dd7d9bd6d3e5e8c32d255a47359e5802eb3fd732
Bobkovsky <dmitrybobkov5@gmail.com>
2022-12-20 00:50:43 +0300

    pprod 20/12

--- src/components/app-menu/app-menu.js
+++ src/components/app-menu/app-menu.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import './app-menu.css';
 import * as Scroll from 'react-scroll';
-import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import { Link } from 'react-scroll'
+
 
 function MainMenu() {
     return(
         <div className='main-menu'>
           <ul>
             <li>
-              <Link to="about" spy={true} smooth={true} duration={500} offset={-50}>
+              <Link id='about1' to="about" smooth={true} duration={200} offset={-150}>
                 О нас
               </Link>
             </li>
             <li>
-              <Link to="ploshadki" spy={true} smooth={true} duration={500} offset={-150}>
+              <Link to="ploshadki" smooth={true} duration={200} offset={-150}>
                 Площадки
               </Link>
             </li>
             <li>
-              <Link to="photoGallery" spy={true} smooth={true} duration={500} offset={-150}>
+              <Link to="photoGallery" smooth={true} duration={200} offset={-150}>
                 Фотогалерея
               </Link>
             </li>
             <li>
-              <Link to="coop" spy={true} smooth={true} duration={500} offset={-150}>
+              <Link to="coop" smooth={true} duration={200} offset={-150}>
                 Сотрудничество              
               </Link>
             </li>
